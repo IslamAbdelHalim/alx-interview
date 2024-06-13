e19ae973f3183763cd8aa739c112f089f1d945a6
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-14 01:20:52 +0300

    a

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,15 +11,13 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    h = 1
-    past = 0
+    factor = 2
     op = 0
 
-    while h < n:
-        if past < h / 2 and h * 2 < n:
-            past = h
-            op += 1
-        h += past
-        op += 1
-
+    while n > 1:
+        while n % factor == 0:
+            op += factor
+            n //= factor
+        factor += 1
+        
     return op
