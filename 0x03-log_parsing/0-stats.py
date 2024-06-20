f377c78df4bcb5aeadf2ff1fa664e9dfc77cec6a
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-21 00:57:37 +0300

    a

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,50 @@
 #!/usr/bin/python3
-"""reads stdin,compute metrics"""
-import re
+
+import sys
 
 
-def get_input(lines):
-    """gets target info from input."""
-    line = lines.split(' ')
-    return {
-        'status_code': line[-2],
-        'file_size': int(line[-1]),
-    }
+def print_msg(dict_sc, total_file_size):
+    """
+    printing data
+    """
+
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
-def print_stats(file_size_sum, status_codes_stats):
-    """Print acumulated statistics of HTTP log"""
-    print('File size: {:d}'.format(file_size_sum), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        n = status_codes_stats.get(status_code, 0)
-        if n > 0:
-            print('{:s}: {:d}'.format(status_code, n), flush=True)
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0
+           }
 
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-def run():
-    """run input parser"""
-    line_n = 0
-    file_size_sum = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
-    try:
-        while True:
-            line = input()
-            line_info = get_input(line)
-            code = line_info.get('status_code', '0')
-            if code in status_codes_stats.keys():
-                status_codes_stats[code] += 1
-            file_size_sum += line_info['file_size']
-            line_n += 1
-            if line_n % 10 == 0:
-                print_stats(file_size_sum, status_codes_stats)
-    except (KeyboardInterrupt, EOFError, SystemExit):
-        print_stats(file_size_sum, status_codes_stats)
+        if len(parsed_line) > 2:
+            counter += 1
 
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-if __name__ == '__main__':
-    run()
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
