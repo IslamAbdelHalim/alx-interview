ac89db4460e805840007f6ff61532ce2b1d99842
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-07-11 20:58:04 +0300

    a

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+const film = args[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${film}/`;
+
+request(url, function(err, res, body) {
+
+    if(err)
+        throw err
+
+    characters = JSON.parse(body).characters;
+
+    for (const character of characters) {
+        request(character, function(err, res, body){
+            console.log(JSON.parse(body).name)
+        })
+    }
+})
